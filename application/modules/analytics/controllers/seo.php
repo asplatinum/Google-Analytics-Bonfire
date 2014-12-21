34e8bf2aa914a6317df892a9aa23898035d7808e
asplatinum <info@blueorb.co.uk>
2014-12-21 19:28:37 +0000

    DB optimizations
    Less calls on DB for the same thing.

--- application/modules/analytics/controllers/seo.php
+++ application/modules/analytics/controllers/seo.php
@@ -21,15 +21,15 @@ class seo extends Admin_Controller
 		$this->auth->restrict('Analytics.Seo.View');
 		$this->lang->load('analytics');
 
-		require_once('../application/third_party/GoogleAnalytics/Client.php');
-		require_once('../application/third_party/GoogleAnalytics/Service.php');
+		require_once(APPPATH.'third_party/Google/Client.php');
+		require_once(APPPATH.'third_party/Google/Service.php');
 
 		Template::set_block('sub_nav', 'seo/_sub_nav');
 		//Assets::add_module_js('analytics', 'analytics.js');
 		
 		$this->GA_config = $this->GA_config();
 
-		$this->analytics = new Google_Service_Analytics($this->GA_login());
+		$this->analytics = new Google_Service_Analytics($this->GA_login($this->GA_config->GA_key_file_location, $this->GA_config()->GA_service_account_name));
 	}
 
 	//--------------------------------------------------------------------
@@ -43,18 +43,20 @@ class seo extends Admin_Controller
 	public function index($startDate_offset=1, $endDate=false)
 	{
 
-		if($this->GA_config) {
+		if(isset($this->GA_config)) {
 		
 			$startDate = date('Y-m-d', strtotime('-'.$startDate_offset.' month')); 
 			$endDate = ($endDate ? strtotime($endDate) : date('Y-m-d'));
 
-			$data['GA_users'] = $this->GA_users($this->GA_config()->GA_profileId, $startDate, $endDate);;
-			$data['GA_browsers'] = $this->GA_browsers($this->GA_config()->GA_profileId, $startDate, $endDate);
-			$data['GA_referrers'] = $this->GA_referrers($this->GA_config()->GA_profileId, $startDate, $endDate);
-			$data['GA_visitors_day'] = $this->GA_visitors_day($this->GA_config()->GA_profileId, $startDate, $endDate);
+			$GA_config = $this->GA_config;
+
+			$data['GA_users'] = $this->GA_users($GA_config->GA_profileId, $startDate, $endDate);;
+			$data['GA_browsers'] = $this->GA_browsers($GA_config->GA_profileId, $startDate, $endDate);
+			$data['GA_referrers'] = $this->GA_referrers($GA_config->GA_profileId, $startDate, $endDate);
+			$data['GA_visitors_day'] = $this->GA_visitors_day($GA_config->GA_profileId, $startDate, $endDate);
 
 		} else {
-			$data['GA_data'] = null;
+			$data['db_settings'] = true;
 		}
 
 				Template::set('GA_data', $data);
@@ -66,11 +68,11 @@ class seo extends Admin_Controller
 
 	
 	private function GA_config() {
-		$query = $this->db->get('bf_ga_config');
+		$query = $this->db->get('ga_config');
 			if ($query->num_rows() > 0) {
 				$row = $query->row(); 
 				$data = (object) array('GA_ClientID' => $row->ga_clientID, 'GA_service_account_name' => $row->ga_svc_acc_name, 
-					'GA_key_file_location' => APPPATH . 'third_party/GoogleAnalytics/' . $row->ga_p12_key, 
+					'GA_key_file_location' => APPPATH . 'third_party/Google/' . $row->ga_p12_key, 
 					'GA_profileId' => 'ga:' . $row->ga_profileID);
 				return $data;
 			} else {
@@ -79,7 +81,7 @@ class seo extends Admin_Controller
 		
 	}
 
-	private function GA_login() {
+	private function GA_login($GA_key_file_location, $GA_service_account_name) {
 		$client = new Google_Client();
 		$client->setApplicationName("This_Was_A_Test");
 		$service = new Google_Service_Analytics($client);
@@ -88,10 +90,10 @@ class seo extends Admin_Controller
 			$client->setAccessToken($this->session->userdata('service_token'));
 		}
 
-		$key = file_get_contents($this->GA_config->GA_key_file_location);
+		$key = file_get_contents($GA_key_file_location);
 
 		$cred = new Google_Auth_AssertionCredentials(
-		$this->GA_config()->GA_service_account_name,
+		$GA_service_account_name,
 			array(
 				'https://www.googleapis.com/auth/analytics'),$key,'notasecret'
 				);
@@ -111,21 +113,14 @@ class seo extends Admin_Controller
 	private function GA_users($profileID, $startDate, $endDate) {
 		$metrics = "ga:sessions";
 		$optParams = array("dimensions" => "ga:userType");
-		$data = $this->analytics->data_ga->get($this->GA_config()->GA_profileId, $startDate, $endDate, $metrics, $optParams);
-
-		$res = [
-            'items' => isset($data['rows']) ? $data['rows'] : [],
-            'columnHeaders' => $data['columnHeaders'],
-            'totalResults'  => $data['totalResults']
-        ];
-        //var_dump($res);
+		$data = $this->analytics->data_ga->get($profileID, $startDate, $endDate, $metrics, $optParams);
 		return $data;
 	}
 
 	private function GA_browsers($profileID, $startDate, $endDate) {
 		$metrics = "ga:sessions";
 		$optParams = array("dimensions" => "ga:browser", "sort" => "-ga:sessions");
-		$data = $this->analytics->data_ga->get($this->GA_config()->GA_profileId, $startDate, $endDate, $metrics, $optParams);
+		$data = $this->analytics->data_ga->get($profileID, $startDate, $endDate, $metrics, $optParams);
 
 		return $data;
 	}
@@ -133,7 +128,7 @@ class seo extends Admin_Controller
 	private function GA_referrers($profileID, $startDate, $endDate) {
 		$metrics = "ga:sessions";
 		$optParams = array("dimensions" => "ga:source, ga:referralPath", "sort" => "-ga:sessions");
-		$data = $this->analytics->data_ga->get($this->GA_config()->GA_profileId, $startDate, $endDate, $metrics, $optParams);
+		$data = $this->analytics->data_ga->get($profileID, $startDate, $endDate, $metrics, $optParams);
 
 		return $data;
 	}
@@ -141,7 +136,7 @@ class seo extends Admin_Controller
 	private function GA_visitors_day($profileID, $startDate, $endDate) {
 		$metrics = "ga:pageviews, ga:sessions";
 		$optParams = array("dimensions" => "ga:date", "sort" => "ga:date");
-		$data = $this->analytics->data_ga->get($this->GA_config()->GA_profileId, $startDate, $endDate, $metrics, $optParams);
+		$data = $this->analytics->data_ga->get($profileID, $startDate, $endDate, $metrics, $optParams);
 
 		return $data;
 	}
